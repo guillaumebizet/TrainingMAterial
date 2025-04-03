e905a2b94a3cd2a9794c04302cb8a592c0e43ef7
guillaumebizet <guillaume.bizet@socgen.com>
2025-04-03 13:32:11 +0200

    Update config.github.com.js

--- config.github.com.js
+++ config.github.com.js
@@ -2,7 +2,7 @@ const githubConfig = {
   apiUrl: "https://api.github.com",
   authUrl: "https://github.com/login/oauth/authorize",
   tokenUrl: "https://github.com/login/oauth/access_token",
-  clientId: "VOTRE_CLIENT_ID_GITHUB_COM", // Remplacez par votre Client ID pour GitHub.com
+  clientId: "Ov23liQj7MXBgBOqNVAE", 
   redirectUri: "https://guillaumebizet.github.io/TrainingMATERIAL/callback",
   repo: "guillaumebizet/TrainingMATERIAL",
   branch: "main",
