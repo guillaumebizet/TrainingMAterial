269098a217f552b98ab6dac4868d095eb353a045
guillaumebizet <guillaume.bizet@socgen.com>
2025-04-03 13:26:00 +0200

    Create config.github.socgen.js

---
+++ config.github.socgen.js
@@ -0,0 +1,12 @@
+const githubConfig = {
+  apiUrl: "https://sgithub.fr.world.socgen/api/v3",
+  authUrl: "https://sgithub.fr.world.socgen/login/oauth/authorize",
+  tokenUrl: "https://sgithub.fr.world.socgen/login/oauth/access_token",
+  clientId: "VOTRE_CLIENT_ID_SOCGEN",
+  redirectUri: "URL_DE_REDIRECTION_SOCGEN",
+  repo: "a474881/training",
+  branch: "coding-main",
+  questionsPath: "questions.json",
+  scoresPath: "scores.json",
+  scope: "repo"
+};
